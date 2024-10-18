302b07dba492b05e875f0a6f61a42035d01dd473
Nags <17843319+Jnagasuresh@users.noreply.github.com>
2024-10-18 14:45:31 +0530

    sample.ps1

---
+++ sample.ps1
@@ -0,0 +1,5 @@
+# Prompt the user to enter their name
+$name = Read-Host "Enter your name"
+
+# Greet the user with a personalized message
+Write-Output "Hello, $name! Welcome to the world of PowerShell!"
